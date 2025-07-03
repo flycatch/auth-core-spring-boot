0aafb40b5caf261df40596932019bd08ca833f78
shree-ni <shreenidhi.m@flycatchtech.com>
2025-07-03 13:28:31 +0530

    un comment to run locally auth core

---
+++ authcore/src/main/java/com/flycatch/authcore/service/AuthCoreUserServiceImpl.java
@@ -0,0 +1,33 @@
+//package com.flycatch.authcore.service;
+//
+//import com.flycatch.authcore.model.User;
+//import com.flycatch.authcore.repository.UserRepository;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Optional;
+//
+//@Service
+//public class AuthCoreUserServiceImpl implements AuthCoreUserService {
+//
+//    private final UserRepository userRepository;
+//
+//    public AuthCoreUserServiceImpl(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+//
+//    @Override
+//    public Optional<User> findByUsername(String username) {
+//        return userRepository.findByUsername(username);
+//    }
+//
+//    @Override
+//    public Optional<User> findByEmail(String email) {
+//        return userRepository.findByEmail(email);
+//    }
+//
+//    @Override
+//    public User save(String username, String email, String encodedPassword) {
+//        User user = new User(username, email, encodedPassword);
+//        return userRepository.save(user);
+//    }
+//}
